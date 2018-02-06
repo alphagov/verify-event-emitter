a3e77f6c9f6b05fc3e80c371480503d2549521db
Michael Walker <michael.walker@softwire.com>
2018-02-06 12:18:47 +0000

    ABD-27: Deliberately break tests to confirm that build will fail.
    Pair: @adityapahuja @michaelwalker

--- src/test/java/uk/gov/ida/eventemitter/EventTest.java
+++ src/test/java/uk/gov/ida/eventemitter/EventTest.java
@@ -6,6 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EventTest {
     @Test
     public void testAdd() {
-        assertThat(1+1).isEqualTo(2);
+        assertThat(1+1).isEqualTo(3);
     }
 }
