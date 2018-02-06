41267feb9e05f7cd179d37340828369eebebba85
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2018-02-06 10:49:10 +0000

    ABD-26: Create a simple test
    Authors: @adityapahuja @michaelwalker

---
+++ README.md
@@ -0,0 +1,11 @@
+# Verify Event Emitter
+
+### Building the project
+
+`./gradlew clean build`
+
+## Licence
+
+[MIT Licence](LICENCE)
+
+This code is provided for informational purposes only and is not yet intended for use outside GOV.UK Verify.
---
+++ src/test/java/uk/gov/ida/eventemitter/EventTest.java
@@ -0,0 +1,11 @@
+package uk.gov.ida.eventemitter;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+    @Test
+    public void testAdd() {
+        assertThat(1+1).isEqualTo(2);
+    }
+}
