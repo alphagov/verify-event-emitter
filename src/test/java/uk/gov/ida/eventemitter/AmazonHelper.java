fa88b36584ade45c7309985792fdb0dcebe392b4
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2018-03-07 15:46:34 +0000

    ABD-40: Add an integration test to cover a missing S3 bucket
    When S3 bucket is missing, Event Emitter should use stub encrypter.
    
    Authors: @adityapahuja

---
+++ src/test/java/uk/gov/ida/eventemitter/AmazonHelper.java
@@ -0,0 +1,99 @@
+package uk.gov.ida.eventemitter;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.AmazonSQSException;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+import com.google.inject.util.Types;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+public class AmazonHelper {
+
+    private AmazonHelper() {}
+
+    public static Message getAMessageFromSqs(final AmazonSQS sqs,
+                                             final String queueUrl) {
+        final ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
+        final List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+
+        return messages.get(0);
+    }
+
+    public static void createSourceQueue(final AmazonSQS sqs,
+                                         final String sourceQueueName) {
+        try {
+            sqs.createQueue(sourceQueueName);
+        } catch (AmazonSQSException e) {
+            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+                throw e;
+            }
+        }
+    }
+
+    public static void setUpS3Bucket(final AmazonS3 s3,
+                                     final String bucketName,
+                                     final String keyName,
+                                     final String key) {
+        try {
+            s3.createBucket(bucketName);
+        } catch (AmazonS3Exception e) {
+            System.err.println(e.getErrorMessage());
+        }
+
+        try {
+            s3.putObject(bucketName, keyName, key);
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+        }
+    }
+
+    public static void deleteBucket(final AmazonS3 s3,
+                                    final String bucketName) {
+        try {
+            ObjectListing object_listing = s3.listObjects(bucketName);
+            while (true) {
+                for (Iterator<?> iterator =
+                     object_listing.getObjectSummaries().iterator();
+                     iterator.hasNext();) {
+                    S3ObjectSummary summary = (S3ObjectSummary)iterator.next();
+                    s3.deleteObject(bucketName, summary.getKey());
+                }
+
+                if (object_listing.isTruncated()) {
+                    object_listing = s3.listNextBatchOfObjects(object_listing);
+                } else {
+                    break;
+                }
+            };
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+        }
+    }
+
+    public static Optional<String> getQueueUrl(final Injector injector) {
+        return (Optional<String>) injector.getInstance(
+            Key.get(TypeLiteral.get(Types.newParameterizedType(Optional.class, String.class)), Names.named("SourceQueueUrl")));
+    }
+
+    public static Optional<AmazonSQS> getInstanceOfAmazonSqs(final Injector injector) {
+        return (Optional<AmazonSQS>) injector.getInstance(
+            Key.get(TypeLiteral.get(Types.newParameterizedType(Optional.class, AmazonSQS.class))));
+    }
+
+    public static Optional<AmazonS3> getInstanceOfAmazonS3(final Injector injector) {
+        return (Optional<AmazonS3>) injector.getInstance(
+            Key.get(TypeLiteral.get(Types.newParameterizedType(Optional.class, AmazonS3.class))));
+    }
+}
