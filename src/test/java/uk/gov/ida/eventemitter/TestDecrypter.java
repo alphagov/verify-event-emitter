a80d55a00ea6c13a268cb35cbd7d8f5ad5a9dd43
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2018-04-16 14:36:12 +0100

    BAU: Replace abbrevations with words for variables
    These changes will help developers to know what these variables are for.
    
    Authors: @adityapahuja

--- src/test/java/uk/gov/ida/eventemitter/TestDecrypter.java
+++ src/test/java/uk/gov/ida/eventemitter/TestDecrypter.java
@@ -8,9 +8,10 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import static uk.gov.ida.eventemitter.EventEncrypter.INITIALISATION_VECTOR_SIZE;
+
 public class TestDecrypter<T> {
 
-    private static final int IV_SIZE = 16;
     private final byte[] key;
     private final ObjectMapper mapper;
 
@@ -23,30 +24,30 @@ public class TestDecrypter<T> {
 
     public T decrypt(final String bas64EncodedEncryptedEvent, final Class<T> klass) throws Exception {
         final byte[] encryptedEventAndIv = Base64.decodeBase64(bas64EncodedEncryptedEvent);
-        final byte[] iv = extractIv(encryptedEventAndIv);
+        final byte[] initialisationVector = extractIv(encryptedEventAndIv);
         final byte[] encryptedEvent = extractEncryptedEvent(encryptedEventAndIv);
-        final byte[] event = decryptEncryptedEvent(encryptedEvent, iv);
+        final byte[] event = decryptEncryptedEvent(encryptedEvent, initialisationVector);
 
         return (T) mapper.readValue(event, klass);
     }
 
     private byte[] extractIv(final byte[] encryptedEventAndIv) {
-        byte[] iv = new byte[IV_SIZE];
-        System.arraycopy(encryptedEventAndIv, 0, iv, 0, iv.length);
-        return  iv;
+        byte[] initialisationVector = new byte[INITIALISATION_VECTOR_SIZE];
+        System.arraycopy(encryptedEventAndIv, 0, initialisationVector, 0, initialisationVector.length);
+        return  initialisationVector;
     }
 
     private byte[] extractEncryptedEvent(final byte[] encryptedEventAndIv) {
-        int encryptedSize = encryptedEventAndIv.length - IV_SIZE;
+        int encryptedSize = encryptedEventAndIv.length - INITIALISATION_VECTOR_SIZE;
         byte[] encryptedEvent = new byte[encryptedSize];
-        System.arraycopy(encryptedEventAndIv, IV_SIZE, encryptedEvent, 0, encryptedSize);
+        System.arraycopy(encryptedEventAndIv, INITIALISATION_VECTOR_SIZE, encryptedEvent, 0, encryptedSize);
         return encryptedEvent;
     }
 
-    private byte[] decryptEncryptedEvent(final byte[] encryptedEvent, final byte[] iv) throws Exception {
+    private byte[] decryptEncryptedEvent(final byte[] encryptedEvent, final byte[] initialisationVector) throws Exception {
         final SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
         final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec, new IvParameterSpec(iv));
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, new IvParameterSpec(initialisationVector));
         return cipher.doFinal(encryptedEvent);
     }
 }
